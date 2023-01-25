9dc3ce0e9e95e87e982ef4fd306c9ce982ff4fa5
kiddden <kazzer@knu.ua>
2023-01-25 15:47:38 +0200

    added documentation to JS script

--- StormotionTestTask/Resources/script.js
+++ StormotionTestTask/Resources/script.js
@@ -1,9 +1,11 @@
+// Method that allows us to send a message to our app with the mata-ml-post-id
 function getPostId(element) {
     window.webkit.messageHandlers.jsMessenger.postMessage(element.getAttribute('data-ml-post-id'));
 }
 
 var postLenght = 0;
 
+// Setting up a listener to each element(article/post) on the document(page) so it sends us data-ml-post-id of the clicked element.
 window.onload = function() {
     let elements = document.getElementsByClassName('list-item');
     for (let item of elements) {
@@ -14,11 +16,13 @@ window.onload = function() {
     postLenght = elements.length;
 }
 
+// Checking whenever the document(webpage) mutates, meaning that new post were loaded
 var observer = new MutationObserver(function(mutations) {
     let elements = document.getElementsByClassName('list-item');
     let elementsArr = Array.from(elements);
     
-    // window.webkit.messageHandlers.jsMessenger.postMessage(elements.slice(postLenght));
+// Splitting array of elements into two slices, and working with the second slice that doesn't include the elements that we already set listeners for.
+// The main purpose of this is to avoid setting listeners to an element that already has one.
     for (let item of elementsArr.slice(postLenght)) {
         item.addEventListener('click', function() {
             getPostId(item);
